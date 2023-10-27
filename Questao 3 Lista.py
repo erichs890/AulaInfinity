9bba9373015af11e6ad9f7914dfefc13f91ddf5a
unknown <erichs890@gmail.com>
2023-10-27 18:47:14 -0300

    Questao 3 foi feita

--- Questao 3 Lista.py
+++ Questao 3 Lista.py
@@ -1,14 +1,20 @@
-# Lista de produtos e preços
+# # Lista de produtos e preços
+# produtos = [("Maçã", 2.50), ("Banana", 1.75), ("Laranja", 3.00)]
+
+# # Inicializa a variável que irá armazenar o valor total
+# total = 0
+
+# # Itera sobre a lista de produtos
+# for produto, preco in produtos:
+#     print(f"{produto}: R${preco:.2f}")
+#     total += preco
+
+# # Exibe o valor total
+# print(f"Valor total: R${total:.2f}")
+# #Feito com o GPT
+
 produtos = [("Maçã", 2.50), ("Banana", 1.75), ("Laranja", 3.00)]
-
-# Inicializa a variável que irá armazenar o valor total
-total = 0
-
-# Itera sobre a lista de produtos
-for produto, preco in produtos:
-    print(f"{produto}: R${preco:.2f}")
-    total += preco
-
-# Exibe o valor total
-print(f"Valor total: R${total:.2f}")
-#Feito com o GPT
+soma = 0
+for produto in produtos:
+    soma += produto[1]
+    print(soma)
