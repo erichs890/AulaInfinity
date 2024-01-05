055a97e0643ab9bbd8a575820e738b21d4229798
erichs890 <114402221+erichs890@users.noreply.github.com>
2024-01-05 16:44:04 -0300

    Update bagulhoTkinter.py

--- bagulhoTkinter.py
+++ bagulhoTkinter.py
@@ -4,29 +4,31 @@ bufu.title("EITA")
 bufu.geometry("800x800")
 
 def verSePassou():
-    media = (float(nota1Input.get()) + float(nota2Input.get()))/2
-    if media >= 7:
+    media = (float(nota1Input.get()) + float(nota2Input.get())+float(nota3Input.get()))/3
+    if media < 10 and media >= 7:
         passouOuNao.configure(text="PASSOU", bg="green")
         bufu.configure(bg="green")
         nota1.configure(bg="green")
-        nota2Input.configure(bg="green")
-
-        nota2Input.configure(bg="green")
-
-
         nota2.configure(bg="green")
+        nota3.configure(bg="green")
         botao.configure(bg="green")
 
-    else:
+    elif media < 7:
          passouOuNao.configure(text="nao passou...", bg="red")
          bufu.configure(bg="red")
          nota1.configure(bg="red")
-         nota1Input.configure(bg="red")
-         nota2Input.configure(bg="red")
-
          nota2.configure(bg="red")
+         nota3.configure(bg="red")
+
          botao.configure(bg="red")
-         
+    elif media == 10:
+        passouOuNao.configure(text="PASSOU COM 10 NA MEDIA", bg="blue")
+        bufu.configure(bg="blue")
+        nota1.configure(bg="blue")
+        nota2.configure(bg="blue")
+        nota3.configure(bg="blue")
+
+        botao.configure(bg="blue")
 
 
 
@@ -45,6 +47,13 @@ nota2.pack()
 
 nota2Input = Entry()
 nota2Input.pack()
+
+nota3 = Label(text="Diz tua terceira nota fofo")
+nota3.pack()
+
+nota3Input = Entry()
+nota3Input.pack()
+
 
 botao = Button(bufu,text="Ver se passou", command=verSePassou)
 botao.pack()
